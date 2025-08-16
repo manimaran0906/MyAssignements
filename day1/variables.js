e2190557fd3e59fe7d6135001f2d80ee906283d1
manimaran0906 <maranc499@gmail.com>
2025-08-16 20:36:49 +0530

    Add files via upload

---
+++ day1/variables.js
@@ -0,0 +1,16 @@
+
+
+let firstName="Manimaran"
+console.log(typeof firstName)
+
+let companyName="CTS"
+console.log(typeof companyName)
+
+let mobileNumber=97865342314
+console.log(typeof mobileNumber)
+
+let isAutomation=true
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
