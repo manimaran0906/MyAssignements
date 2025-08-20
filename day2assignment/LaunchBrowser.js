8cde0c53755c8b9c50781af5916a7c1562bf8716
manimaran0906 <maranc499@gmail.com>
2025-08-20 22:45:54 +0530

    Add files via upload

---
+++ day2assignment/LaunchBrowser.js
@@ -0,0 +1,15 @@
+function launchBrowser(browserName)
+{
+if(browserName==="chrome"){
+    
+    console.log("Launchbrowserwith" + browserName)
+
+}
+else
+{
+    
+    console.log("launchbrowserwith" +  browserName)
+}
+}
+launchBrowser("chrome")
+launchBrowser("edge")
---
+++ day2assignment/runTests.js
@@ -0,0 +1,19 @@
+let testType ="smoke"
+switch(testType){
+    case "smoke":
+        console.log("testType would be smoke")
+
+    case "sanity":
+         console.log("testtype would be sanity")
+
+    case "regression":
+         console.log("testtype would be regression")
+
+    default:
+        console.log("testtype would be smoke")
+      
+                 
+
+
+        
+}
